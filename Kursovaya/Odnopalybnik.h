b623a89d9199ac8c36ed225f9a4a9c4765ed3bea
IlyaPaks <133762461+IlyaPaks@users.noreply.github.com>
2023-05-17 18:27:01 +0700

    Update Odnopalybnik.h

--- Kursovaya/Odnopalybnik.h
+++ Kursovaya/Odnopalybnik.h
@@ -1,1 +1,19 @@
+#pragma once
+#include "kletka.h"
+#include "pole.h"
 
+
+class Odnopalybnik: public kletka
+{
+private:
+	sost palyba;//Őđŕíčň číôîđěŕöčţ î ńîńňî˙íčč ďŕëóáű ęîđŕáë˙
+	//sost korabl; //Őđŕíčň číôîđěŕöčţ î ńîńňî˙íčč ęîđŕáë˙
+public:
+	Odnopalybnik();
+	virtual int ustanovkakorablya(pole* doska); //Óńňŕíîâęŕ íŕ ďîëĺ îäíîďŕëóáíčęŕ
+	virtual sost getpalyba(); //getpalyba íĺ íóćĺí ěíîăîďŕëóáíčęó
+	virtual void setpalyba(sost p);//setpalyiba íĺ íóćĺí ěíîăîďŕëóáíčęó
+	virtual sost proverkakorablya(int x, int y); //Ńďđŕřčâŕĺň ęîđŕáëü ďîďŕëč ëč â íĺăî
+	virtual sost getkorabl();
+	virtual void setkorabl(sost k);
+};
