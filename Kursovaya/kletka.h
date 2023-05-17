2c552d36f3183e2891a300d5dafcefa41176672f
IlyaPaks <133762461+IlyaPaks@users.noreply.github.com>
2023-05-17 18:31:13 +0700

    Update kletka.h

--- Kursovaya/kletka.h
+++ Kursovaya/kletka.h
@@ -1,1 +1,12 @@
+#pragma once
+#include "koordinata.h"
+class kletka : public koordinata
+{
+private:
+	 sostoyanie klet; //Őđŕíčň ńîńňî˙íčĺ ęëĺňęč
+public:
+	kletka();
+	void setklet(sostoyanie k);
+	sostoyanie getklet();
 
+};
