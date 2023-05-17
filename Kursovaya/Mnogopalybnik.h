5960622af270071a2422e3d761da38eb89981b99
IlyaPaks <133762461+IlyaPaks@users.noreply.github.com>
2023-05-17 18:25:31 +0700

    Update Mnogopalybnik.h

--- Kursovaya/Mnogopalybnik.h
+++ Kursovaya/Mnogopalybnik.h
@@ -1,1 +1,18 @@
+#pragma once
+#include "Odnopalybnik.h"
 
+class Mnogopalybnik : public Odnopalybnik
+{
+private: 
+	Odnopalybnik* bolkorabl[4]; //Многопалубник это массив однопалубников
+	int kolpalyb;	//Хранит количество палуб
+public:
+	Mnogopalybnik(); //По умолчанию создается двухпалубный корабль
+	Mnogopalybnik(int k); //Конструктор с параметров количество палуб
+	//sost getpalyba(); //Перекрыл методы однопалубника
+	//void setpalyba(sost p);
+	sost getkorabl();
+	//void setkorabl(sost k);
+	int ustanovkakorablya(pole* doska); //Установка на поле многопалубника
+	sost proverkakorablya(int x, int y); //Спрашивает корабль попали ли в него
+};
