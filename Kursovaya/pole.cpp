b6315658150da6093d4a4c2e247b2404a3e6e5ae
IlyaPaks <133762461+IlyaPaks@users.noreply.github.com>
2023-05-17 18:33:07 +0700

    Update pole.cpp

--- Kursovaya/pole.cpp
+++ Kursovaya/pole.cpp
@@ -1,1 +1,19 @@
+#include "pole.h"
 
+pole::pole()
+{
+	imyapolya = "";
+};
+
+void pole::chistka() //×čńňčň ďîëĺ
+{
+	int i, j;//Ďĺđĺěĺííűĺ ń÷ĺň÷čęč
+	for (i = 0; i < N; i++) 
+	{
+		for (j = 0; j < N; j++) 
+		{
+			pol[i][j].setklet(PUST);
+			pol[i][j].setkoordinata(i, j);
+		}
+	}
+};
