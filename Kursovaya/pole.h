347720c5234a5f7adbbc7e857f561d52d8ac3ffd
IlyaPaks <133762461+IlyaPaks@users.noreply.github.com>
2023-05-17 18:33:36 +0700

    Update pole.h

--- Kursovaya/pole.h
+++ Kursovaya/pole.h
@@ -1,1 +1,13 @@
+#pragma once
+#include "kletka.h"
+class pole 
+{
+public:
+	string imyapolya; // Хранит копию имени игрока
+	string koorposlxoda; // Хранит копию координаты последнего хода
+	string resposlxoda; // Хранит копию  результата последнего хода
+	kletka pol[NMAX][NMAX]; //Хранит информацию о клетках
+	pole();
+	void chistka(); //Чистит поля
+};
 
