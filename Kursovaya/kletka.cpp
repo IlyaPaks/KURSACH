2d35d786cb2b5e81c1b9e5ceb93e029965aa5ed7
IlyaPaks <133762461+IlyaPaks@users.noreply.github.com>
2023-05-17 18:30:39 +0700

    Update kletka.cpp

--- Kursovaya/kletka.cpp
+++ Kursovaya/kletka.cpp
@@ -1,1 +1,16 @@
+#include "kletka.h"
 
+kletka::kletka() //Êîíñòðóêòîð
+{
+	klet = PUST;
+};
+
+void kletka::setklet(sostoyanie k) //óñòàíàâëèâàåò ñîñòîÿíèå êëåòêè
+{
+	klet = k;
+};
+
+sostoyanie kletka::getklet() //ñ÷èòàòü ñîñòîÿíèå êëåòêè
+{
+	return klet;
+};
